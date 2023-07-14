3832eee3875e9e7b0a733e7fb616b3c789874f9a
grant19605 <grant19605@gmail.com>
2023-07-13 22:38:22 -0400

    Test nav

---
+++ my-website/src/app/navigation/top-nav-bar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-nav-bar',
+  templateUrl: './top-nav-bar.html',
+  styleUrls: ['./top-nav-bar.css']
+})
+export class TopNavBarComponent {
+
+}
